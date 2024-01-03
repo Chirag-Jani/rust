ff9f5206f79ea9252db0f85cf0c57c990c8e74a4
Chirag-Jani <chirag.jani59001@gmail.com>
2024-01-03 16:32:49 +0530

    feat: ownership added

---
+++ ownership/src/main.rs
@@ -0,0 +1,34 @@
+fn main() {
+    // let mut s = String::from("hello");
+
+    // println!("'{}'", s);
+
+    // takes_ownership(&mut s);
+
+    // println!("'{}'", s);
+
+    // let int: u32 = 22;
+    // // this won't take ownership as copy is applied to int and uint
+    // demo_func(int);
+    // println!("'{}'", int);
+
+    // length demo
+    let mut str = String::from("Hello");
+    println!("The string `{}` has {} bytes.", str, find_length(&mut str));
+}
+
+// fn takes_ownership(some_string: &mut String) {
+//     some_string.push_str("    Jani   ");
+//     println!("Value in function {:?}", some_string);
+// }
+
+// fn demo_func(s   ome_int: u32) {
+//     // 这个函数接收一个整型参数，并返
+//     println!("Some unsigned integer: {}", some_int);
+// }
+
+fn find_length(s: &mut String) -> usize {
+    s.push_str("Jani");
+    let leng = s.len();
+    leng
+}
