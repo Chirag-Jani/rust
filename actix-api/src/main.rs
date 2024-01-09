55297c3eed25b78c87b0950b2d11f26c6edaeb67
Chirag-Jani <chirag.jani59001@gmail.com>
2024-01-09 15:03:32 +0530

    feat: passing state in apis

--- actix-api/src/main.rs
+++ actix-api/src/main.rs
@@ -1,8 +1,12 @@
 use actix_web::{ get, web, App, HttpResponse, HttpServer, Responder };
 
-#[get("/test")]
-async fn test() -> impl Responder {
-    HttpResponse::Ok().body("Hellow from Test!!! :)")
+struct AppliactionState {
+    app_name: String,
+}
+
+struct AnotherState {
+    is_another: bool,
+    built_who: String,
 }
 
 #[get("/path")]
@@ -10,15 +14,31 @@ async fn path() -> impl Responder {
     HttpResponse::Ok().body("Hey there from path!")
 }
 
-async fn index() -> impl Responder {
-    "Hello World"
+async fn index(
+    data: web::Data<AppliactionState>,
+    data2: web::Data<AnotherState>
+) -> impl Responder {
+    let app_name = &data.app_name;
+    let creator = &data2.built_who;
+    let state = &data2.is_another;
+    format!("Hello from {app_name} which is build by {creator}, true?? {state}")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(test)
+            .app_data(
+                web::Data::new(AppliactionState {
+                    app_name: String::from("Test app"),
+                })
+            )
+            .app_data(
+                web::Data::new(AnotherState {
+                    is_another: true,
+                    built_who: String::from("Jani"),
+                })
+            )
             .service(path)
             .service(web::scope("/app").route("/index.html", web::get().to(index)))
     })
