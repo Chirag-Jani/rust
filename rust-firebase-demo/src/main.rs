970467d716ca16a98e6a6ab66422698ff2d720e0
Chirag-Jani <chirag.jani59001@gmail.com>
2024-01-10 18:29:24 +0530

    feat: add user, get all user done

--- rust-firebase-demo/src/main.rs
+++ rust-firebase-demo/src/main.rs
@@ -1,7 +1,9 @@
 use firebase_rs::*;
 use serde::{ Serialize, Deserialize };
+use actix_web::{ get, post, web, App, HttpResponse, HttpServer, Responder };
+use std::collections::HashMap;
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 struct User {
     name: String,
     email: String,
@@ -12,28 +14,16 @@ struct Response {
     response: String,
 }
 
+// helper to create user
 async fn add_user(firebase_client: &Firebase, user: &User) -> Response {
     let at_collection = firebase_client.at("users");
 
     let created_user = at_collection.set::<User>(&user).await;
 
-    // println!("{:?}", created_user);
-    // match created_user {
-    //     Ok(_) => println!("Successfully added new user! {}", created_user.unwrap().data),
-    //     Err(e) => eprintln!("Failed to add user: {}", e),
-    // }
-
     return Response { response: created_user.unwrap().data };
 }
 
-#[tokio::main]
-async fn main() {
-    // demo struct for temp.
-    let user = User {
-        name: "Chirag".to_string(),
-        email: "jani@test.com".to_string(),
-    };
-
+fn get_firebase_instance() -> Firebase {
     // to connect with Firebase realtime database
     // Retrieve Firebase URL from environment variable
     let firebase_url = std::env
@@ -46,19 +36,74 @@ async fn main() {
         panic!("Failed to create Firebase instance: {}", err)
     );
 
+    return firebase;
+}
+
+#[get("/get-user")]
+async fn get_user(name: web::Form<HashMap<String, String>>) -> HttpResponse {
+    let fb = get_firebase_instance();
+    let data = fb.at("users").at(&name.0["name"]);
+    let user = data.get::<User>().await;
+    println!("{:?}", user);
+
+    match data.get::<User>().await {
+        Ok(user) => {
+            // Data was found
+            println!("Got the user: {:?}", user);
+            HttpResponse::Ok().body(format!("Got the name: {:?}", name.0["name"]))
+        }
+
+        Err(err) => {
+            // Log other errors for further investigation
+            println!("Firebase Error: {:?}", err);
+            // Return an appropriate response, e.g., Internal Server Error
+            HttpResponse::InternalServerError().finish()
+        }
+    }
+}
+
+#[get("/get-all")]
+async fn get_all_users() -> impl Responder {
+    let fb = get_firebase_instance();
+    let firebase = fb.at("users");
+    let users_result: Result<HashMap<String, User>, _> = firebase.get().await;
+
+    match users_result {
+        Ok(users) => {
+            // Successfully retrieved users, respond with JSON
+            HttpResponse::Ok()
+                .content_type("application/json")
+                .body(serde_json::to_string(&users).unwrap())
+        }
+        Err(err) => {
+            // Handle the error, for example, log it and respond with an error status
+            println!("Error retrieving users: {}", err);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
+}
+
+#[post("/add-user")]
+async fn create_user(user: web::Form<User>) -> impl Responder {
+    let fb = get_firebase_instance();
+
     // creating user
-    let res = add_user(&firebase, &user).await;
-    println!("{:?}", res);
+    let _res = add_user(&fb, &user).await;
+    // println!("{:?}", res);
+
+    // Handle the user data as needed
+    let response_json = serde_json::to_string(&user).unwrap();
+
+    HttpResponse::Ok().content_type("application/json").body(response_json)
 }
 
-// convert a string to a response
-#[allow(dead_code)]
-fn string_to_response(s: &str) -> Response {
-    serde_json::from_str(s).unwrap()
-}
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(move || {
+        App::new().service(get_all_users).service(create_user).service(get_user)
+    })
+        .bind(("127.0.0.1", 8080))?
+        .run().await?;
 
-//convert a string to a user
-#[allow(dead_code)]
-fn string_to_user(s: &str) -> User {
-    serde_json::from_str(s).unwrap()
+    Ok(())
 }
