3b6414191e5791e046c3dfe0f6e29ff74ed11ea4
Chirag-Jani <chirag.jani59001@gmail.com>
2024-02-07 13:25:15 +0530

    checkpoint: impl

--- zero-to-mastery/src/main.rs
+++ zero-to-mastery/src/main.rs
@@ -10,10 +10,32 @@ impl GroceryItem {
     fn print_quantity(data: &GroceryItem) {
         println!("Quantity: {}", data.quantity);
     }
+    fn new_item() -> Self {
+        Self {
+            id: 0,
+            quantity: 0,
+        }
+    }
 }
 
 fn main() {
     let demo = GroceryItem { id: 9387654, quantity: 43 };
     demo.print_id();
-    GroceryItem::print_quantity(&demo)
+    GroceryItem::print_quantity(&demo);
+
+    let demo2 = GroceryItem::new_item();
+    demo2.print_id();
 }
+
+/*
+- variables & data types
+- if else
+- match case
+- loops (for-loop & loop keyword, while revise)
+- functions
+- enums
+- tuple (skipped, feels easy)
+- structs
+- impl (basic)
+- ownership (borrows)
+*/
