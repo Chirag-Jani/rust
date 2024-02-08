55c10bdd54f0959a068701af2dc66f547ab78939
Chirag-Jani <chirag.jani59001@gmail.com>
2024-02-08 12:19:05 +0530

    checkpoint: vector with struct and impl

--- zero-to-mastery/src/main.rs
+++ zero-to-mastery/src/main.rs
@@ -1,85 +1,31 @@
-#[derive(Debug)]
 #[allow(dead_code)]
-enum Color {
-    Red,
-    Blue,
-    Green,
-}
-
-impl Color {
-    fn print(&self) {
-        match self {
-            Color::Blue => {
-                println!("The box is blue.");
-            }
-            Color::Green => {
-                println!("The box is green.");
-            }
-            Color::Red => {
-                println!("The box is red.");
-            }
-        }
-    }
-}
-
 #[derive(Debug)]
-#[allow(dead_code)]
-struct Dimensions {
-    length: i32,
-    width: i32,
-    bredth: i32,
+struct Test {
+    student: String,
+    score: i32,
 }
 
-impl Dimensions {
-    fn square() -> Self {
-        Dimensions {
-            length: 10,
-            width: 10,
-            bredth: 10,
+impl Test {
+    fn new(student: String, score: i32) -> Self {
+        Test {
+            student,
+            score,
         }
-    }
-    fn print(&self) {
-        println!("{:?}", self);
-    }
-}
-
-struct ShippingBox {
-    dimension: Dimensions,
-    weight: i32,
-    color: Color,
-}
-
-impl ShippingBox {
-    fn new(_dimension: Dimensions, _weight: i32, _color: Color) -> Self {
-        ShippingBox {
-            dimension: _dimension,
-            weight: _weight,
-            color: _color,
-        }
-    }
-
-    fn get_shipment(&self) {
-        println!("Getting shipment...");
-        self.dimension.print();
-        println!("\nWeight  : {:?}", self.weight);
-        self.color.print();
     }
 }
 
 fn main() {
-    let new = ShippingBox::new(Dimensions::square(), 33, Color::Red);
-    new.get_shipment();
-}
+    let scores = vec![
+        Test::new("Ron".to_string(), 58),
+        Test::new("John".to_string(), 75),
+        Test::new("AMy".to_string(), 24)
+    ];
 
-/*
-- variables & data types
-- if else
-- match case
-- loops (for-loop & loop keyword, while revise)
-- functions
-- enums
-- tuple (skipped, feels easy)
-- structs
-- impl (basic)
-- ownership (borrows)
-*/
+    for score in scores {
+        if score.score < 34 {
+            println!("{} Failed with Percentage {}", score.student, score.score);
+        } else {
+            println!("{} Passed with Percentage {}", score.student, score.score);
+        }
+    }
+}
