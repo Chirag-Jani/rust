347ef6a12740a0afc5c97f42561fc5f7a46f053c
Chirag-Jani <chirag.jani59001@gmail.com>
2024-02-09 16:51:35 +0530

    exer: plain Result<Ok, Err> demo

--- zero-to-mastery/src/main.rs
+++ zero-to-mastery/src/main.rs
@@ -1,19 +1,41 @@
-#[derive(Debug)]
 #[allow(dead_code)]
-struct Res {
-    status: u8,
-    msg: String,
+#[derive(Debug)]
+struct Adult {
+    name: String,
+    age: u8,
 }
 
-fn get_data(password: String) -> Result<Res, String> {
-    if password == "Jani" {
-        Ok(Res { status: 200, msg: "Done".to_owned() })
-    } else {
-        Err("Oppsie Daiseyyy".to_owned())
+impl Adult {
+    fn new(name: String, age: u8) -> Result<Adult, String> {
+        if age > 21 {
+            Ok(Self { name, age })
+        } else {
+            Err("Not an Adult as AGe is < 21".to_string())
+        }
     }
 }
 
 fn main() {
-    let d = get_data("Chirag".to_string());
-    println!("The data received is: {:?}", d)
+    let mut members: Vec<Adult> = Vec::new();
+
+    let ad1 = Adult::new("Chirag".to_owned(), 10);
+    let ad2 = Adult::new("Jani".to_owned(), 100);
+
+    match ad1 {
+        Ok(adult) => {
+            println!("{:?}", adult);
+            members.push(adult);
+        }
+        Err(reason) => println!("{:?}", reason),
+    }
+
+    match ad2 {
+        Ok(adult) => {
+            println!("{:?}", adult);
+            members.push(adult);
+        }
+        Err(reason) => println!("{:?}", reason),
+    }
+
+    println!("The final list of adults is: {:?}", members)
 }
