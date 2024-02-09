2496250937ceaeb0a50e7bf80f00818316a25fd1
Chirag-Jani <chirag.jani59001@gmail.com>
2024-02-09 17:13:20 +0530

    checkpoint: hashmap and ? todo

--- zero-to-mastery/src/main.rs
+++ zero-to-mastery/src/main.rs
@@ -1,41 +1,8 @@
-#[allow(dead_code)]
-#[derive(Debug)]
-struct Adult {
-    name: String,
-    age: u8,
-}
-
-impl Adult {
-    fn new(name: String, age: u8) -> Result<Adult, String> {
-        if age > 21 {
-            Ok(Self { name, age })
-        } else {
-            Err("Not an Adult as AGe is < 21".to_string())
-        }
-    }
-}
+use std::collections::HashMap;
 
 fn main() {
-    let mut members: Vec<Adult> = Vec::new();
-
-    let ad1 = Adult::new("Chirag".to_owned(), 10);
-    let ad2 = Adult::new("Jani".to_owned(), 100);
-
-    match ad1 {
-        Ok(adult) => {
-            println!("{:?}", adult);
-            members.push(adult);
-        }
-        Err(reason) => println!("{:?}", reason),
-    }
-
-    match ad2 {
-        Ok(adult) => {
-            println!("{:?}", adult);
-            members.push(adult);
-        }
-        Err(reason) => println!("{:?}", reason),
-    }
-
-    println!("The final list of adults is: {:?}", members)
+    let mut members: HashMap<String, String> = HashMap::new();
+    members.insert("Jani".to_owned(), "INtern".to_owned());
+    members.insert("CHirag Limb".to_owned(), "TL".to_owned());
+    println!("The final list of adults is: {:?}", members);
 }
