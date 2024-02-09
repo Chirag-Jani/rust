80a03b0724235b0557eefede5bcf58d519424ccd
Chirag-Jani <chirag.jani59001@gmail.com>
2024-02-09 12:16:16 +0530

    checkpoint: advance match demo

--- zero-to-mastery/src/main.rs
+++ zero-to-mastery/src/main.rs
@@ -1,28 +1,39 @@
-fn main() {
-    // first approach
-    // let numbers = vec![10, 20, 30, 40];
+#[allow(dead_code)]
+struct TickData {
+    name: String,
+    price: u32,
+}
 
-    // another way to do
-    let mut numbers = Vec::new();
-    numbers.push(10);
-    numbers.push(20);
-    numbers.push(30);
-    numbers.push(40);
-
-    for n in &numbers {
-        // first approach
-        // if n == 30 {
-        //     println!("Thirty");
-        // } else {
-        //     println!("{}", n);
-        // }
-
-        // could have used match
-        match n {
-            30 => println!("Thirty"),
-            _ => println!("{}", n),
-        }
+impl TickData {
+    fn new(name: String, price: u32) -> Self {
+        TickData { name, price }
     }
 
-    println!("Total elements in your vector: {}", numbers.len());
+    fn print_ticket(&self) {
+        println!("The Ticket holder is {} and it's price is {}.", self.name, self.price)
+    }
+}
+
+enum Ticket {
+    Backstage(TickData),
+    Vip(TickData),
+    Standard(u32),
+}
+
+fn main() {
+    let mut tickets: Vec<Ticket> = Vec::new();
+
+    tickets.push(Ticket::Backstage(TickData::new("Chirag Jani".to_string(), 999)));
+    tickets.push(Ticket::Vip(TickData::new("Jon".to_string(), 899)));
+    tickets.push(Ticket::Standard(499));
+
+    for ticket in tickets {
+        match ticket {
+            Ticket::Backstage(data) => data.print_ticket(),
+            Ticket::Vip(data) => data.print_ticket(),
+            Ticket::Standard(price) => {
+                println!("This is a Standard Ticket and it's price is {}.", price);
+            }
+        }
+    }
 }
