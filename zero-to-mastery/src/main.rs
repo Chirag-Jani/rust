6ce6ccf65b5083c8cc69470a5b3a1d4970953236
Chirag-Jani <chirag.jani59001@gmail.com>
2024-02-09 15:35:23 +0530

    demo: options

--- zero-to-mastery/src/main.rs
+++ zero-to-mastery/src/main.rs
@@ -4,32 +4,19 @@ struct User {
     email: String,
 }
 
-impl User {
-    fn new(age: f32, email: String) -> Self {
-        User {
-            age: Some(age),
-            email,
-        }
-    }
-}
-
 fn main() {
-    let mut users: Vec<User> = Vec::new();
-
     let us1 = User {
         age: Some(23.0),
         email: "test@mail.com".to_owned(),
     };
 
-    users.push(us1);
-    users.push(User::new(45.0, "email@example.com".to_string()));
-    users.push(User { age: None, email: "hehe@heeh.com".to_owned() });
-
-    for user in users {
-        match user {
-            User { age, email } => {
-                println!("Age is: {:?} and Mail is : {:}", age, email);
+    match us1.age {
+        Some(age) => {
+            match age {
+                18.0 => println!("Teenager at age: {}", 18.0),
+                _ => println!("Naahh"),
             }
         }
+        None => println!("Age not provided"),
     }
 }
