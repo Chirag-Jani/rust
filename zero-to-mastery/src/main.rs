b86a8c641f6505e450268f917885f27d34dc6603
Chirag-Jani <chirag.jani59001@gmail.com>
2024-02-09 15:57:55 +0530

    feat: result opt demo

--- zero-to-mastery/src/main.rs
+++ zero-to-mastery/src/main.rs
@@ -1,22 +1,19 @@
+#[derive(Debug)]
 #[allow(dead_code)]
-struct User {
-    age: Option<f32>,
-    email: String,
+struct Res {
+    status: u8,
+    msg: String,
+}
+
+fn get_data(password: String) -> Result<Res, String> {
+    if password == "Jani" {
+        Ok(Res { status: 200, msg: "Done".to_owned() })
+    } else {
+        Err("Oppsie Daiseyyy".to_owned())
+    }
 }
 
 fn main() {
-    let us1 = User {
-        age: Some(23.0),
-        email: "test@mail.com".to_owned(),
-    };
-
-    match us1.age {
-        Some(age) => {
-            match age {
-                18.0 => println!("Teenager at age: {}", 18.0),
-                _ => println!("Naahh"),
-            }
-        }
-        None => println!("Age not provided"),
-    }
+    let d = get_data("Chirag".to_string());
+    println!("The data received is: {:?}", d)
 }
