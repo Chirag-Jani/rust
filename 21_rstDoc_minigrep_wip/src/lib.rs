8507f73d1dff2757def4d08912fd765900cb057a
Chirag-Jani <chirag.jani59001@gmail.com>
2024-02-13 13:16:50 +0530

    feat: docs way to read using TDD: Test Driven Development

--- 21_rstDoc_minigrep_wip/src/lib.rs
+++ 21_rstDoc_minigrep_wip/src/lib.rs
@@ -21,22 +21,58 @@ impl Config {
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let data = fs::read_to_string(config.filepath)?; // ? does the error handling and returns whatever error might occur
+    // ? does the error handling and returns whatever error might occur
+    let data = fs::read_to_string(config.filepath)?;
+
+    for line in search(&config.query, &data) {
+        println!("{line}");
+    }
 
     // println!("\n\nFinding {:?} in the below", config.query);
     // println!("File Content:\n\n{data}");
 
-    let split_data: Vec<_> = data.split(' ').collect();
-    println!("\n\n\n{:?}\n\n\n", split_data);
+    // BELOW MY CODE - NOT FROM THE BOOK
+    // let split_data: Vec<_> = data.split(' ').collect();
+    // println!("\n\n\n{:?}\n\n\n", split_data);
 
-    for word in split_data {
-        if word == config.query {
-            println!("Word found!!!");
-            return Ok(());
+    // for word in split_data {
+    //     if word == config.query {
+    //         println!("Word found!!!");
+    //         return Ok(());
+    //     }
+    // }
+
+    // println!("Word not found!!!");
+
+    Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            // do something with line
+            results.push(line);
+            println!("Found in line: {line}")
         }
     }
 
-    println!("Word not found!!!");
+    results
+}
 
-    Ok(())
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
