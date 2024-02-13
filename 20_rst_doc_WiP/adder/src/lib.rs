5caff9eb8f2d2ca6da97b9f1e018b5f21e0020ff
Chirag-Jani <chirag.jani59001@gmail.com>
2024-02-13 12:25:47 +0530

    feat: test module from the book done , CHEATSHEET

--- 20_rst_doc_WiP/adder/src/lib.rs
+++ 20_rst_doc_WiP/adder/src/lib.rs
@@ -1,8 +1,10 @@
 #[derive(Debug)]
+#[allow(dead_code)]
 struct Guess {
     value: i32,
 }
 
+#[allow(dead_code)]
 impl Guess {
     fn new(value: i32) -> Result<Self, String> {
         if value < 1 {
@@ -17,8 +19,40 @@ mod tests {
     use super::*;
 
     #[test]
-    #[should_panic(expected = "This is the expected message")]
-    fn guess() {
-        let g = Guess::new(-1).unwrap();
+    fn guess() -> Result<(), String> {
+        let g = Guess::new(1).unwrap();
+        if g.value == 1 {
+            println!("DOne");
+            Ok(())
+        } else {
+            Err(String::from("Invalid Value"))
+        }
+    }
+
+    #[test]
+    #[ignore]
+    fn guess_another() -> Result<(), String> {
+        let g = Guess::new(1).unwrap();
+        if g.value == 1 {
+            println!("DOne");
+            Ok(())
+        } else {
+            Err(String::from("Invalid Value"))
+        }
     }
 }
+
+// CHEAT_SHEET
+// #[cfg(test)] annotation on the tests module tells Rust to compile and run the test code only when you run cargo test, not when you run cargo build.
+// #[test] -> makes the function as test and runs when `cargo test`
+// cargo test -- --show-output -> to print irrespective the test passes or fails
+// #[should_panic(expected = "your_reason")] -> panics with your reason
+// assert_eq!(left, right, "reason") -> checks equality
+// assert_ne!(left, right. "reason") -> checks inequality
+// assert!(value) -> checks trueness
+// cargo test -- --test-threads=1 -> runs tests on one thread, in case of the inter-dependencies
+// #[ignore] -> to ignore the test
+// cargo test <test_name> -> will run the test if name matches
+// cargo test -- --include-ignored -> include ignored tests
+// cargo test --help -> help regarding the command
+// cargo test -- --help -> help regarding what can be passed after "--" (--ignored -> to run the ignored tests)
