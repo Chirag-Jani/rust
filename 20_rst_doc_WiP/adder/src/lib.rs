37391ba0d099b01651ff71ce1070de90684a063c
Chirag-Jani <chirag.jani59001@gmail.com>
2024-02-13 11:09:44 +0530

    feat: should_panic

--- 20_rst_doc_WiP/adder/src/lib.rs
+++ 20_rst_doc_WiP/adder/src/lib.rs
@@ -1,12 +1,14 @@
-struct Rect {
-    height: u32,
-    width: u32,
+#[derive(Debug)]
+struct Guess {
+    value: i32,
 }
 
-#[allow(dead_code)]
-impl Rect {
-    fn is_valid(&self) -> bool {
-        self.height != self.width
+impl Guess {
+    fn new(value: i32) -> Result<Self, String> {
+        if value < 1 {
+            return Err("Invalid Value".to_owned());
+        };
+        Ok(Self { value })
     }
 }
 
@@ -15,14 +17,9 @@ mod tests {
     use super::*;
 
     #[test]
-    fn is_valid_rect() {
-        let _rect = Rect {
-            height: 32,
-            width: 52,
-        };
-
-        assert!(rect.is_valid())
-        // assert_ne!(2, 2, "Both values are same which should not be");
-        // assert_eq!(2, 2, "Both values are not same which should be");
+    #[should_panic]
+    fn guess() {
+        let g = Guess::new(23).unwrap();
+        assert_eq!(g.value, 23, "value mismatched")
     }
 }
