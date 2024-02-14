46b19ed308b47ed07c1fe4cf5c7739143aca7fb6
Chirag-Jani <chirag.jani59001@gmail.com>
2024-02-14 13:28:12 +0530

    feat: lifetime with generic tyep demo

--- 20_rst_doc_WiP/src/main.rs
+++ 20_rst_doc_WiP/src/main.rs
@@ -1,43 +1,24 @@
-use std::result;
+use std::fmt::Display;
 
-fn longest<'a, 'zzz>(x: &'a str, y: &'a str, w: &'zzz str, z: &'zzz str) -> (&'a str, &'zzz str) {
-    let r1;
-    let r2;
-
+fn longest<'a, GenericType>(x: &'a str, y: &'a str, name: GenericType) -> &'a str
+where
+    GenericType: Display,
+{
+    println!("This function is called by {name}");
     if x.len() > y.len() {
-        r1 = x;
+        x
     } else {
-        r1 = y;
+        y
     }
-
-    if w.len() > z.len() {
-        r2 = w;
-    } else {
-        r2 = z;
-    }
-
-    (r1, r2)
 }
 
 fn main() {
     let string1 = String::from("abcd");
     let string2 = "xyz";
 
-    let string4 = "this is longer than string3";
-
     let result;
-    let result2;
 
-    {
-        let string3 = String::from("abasdfaldcd");
-        (result, result2) = longest(string1.as_str(), string2, &string3.as_str(), string4);
-        // the smallest lifetime is assigned and thus this will give us below error
-        // `string3` does not live long enough
-        // borrowed value does not live long enoughrustcClick for full compiler diagnostic
-        // main.rs(35, 5): `string3` dropped here while still borrowed
-        // main.rs(32, 13): binding `string3` declared here
-        // main.rs(37, 58): borrow later used here
-    }
+    (result) = longest(string1.as_str(), string2, "User2");
 
-    println!("The longest string is: {} and {}", result, result2);
+    println!("The longest string is: {}", result);
 }
