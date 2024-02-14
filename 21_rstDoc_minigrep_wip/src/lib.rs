55f4016015c0ca58702d1c5dd9c9672e976a9c9a
Chirag-Jani <chirag.jani59001@gmail.com>
2024-02-14 15:06:06 +0530

    refactor: ignore case w/o env

--- 21_rstDoc_minigrep_wip/src/lib.rs
+++ 21_rstDoc_minigrep_wip/src/lib.rs
@@ -5,6 +5,7 @@ use std::fs;
 pub struct Config {
     pub query: String,
     pub filepath: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
@@ -14,9 +15,20 @@ impl Config {
         };
 
         let query = args[1].clone();
-        let filepath = args[2].clone();
+        let filepath: String = args[2].clone();
+        let ignore_case;
 
-        Ok(Self { query, filepath })
+        if args[3] == "true" {
+            ignore_case = true;
+        } else {
+            ignore_case = false;
+        }
+
+        Ok(Self {
+            query,
+            filepath,
+            ignore_case,
+        })
     }
 }
 
@@ -24,8 +36,14 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     // ? does the error handling and returns whatever error might occur
     let data = fs::read_to_string(config.filepath)?;
 
-    for line in search(&config.query, &data) {
-        println!("{line}");
+    if config.ignore_case == true {
+        for line in search_insensitive(&config.query, &data) {
+            println!("{line}");
+        }
+    } else {
+        for line in search(&config.query, &data) {
+            println!("{line}");
+        }
     }
 
     // println!("\n\nFinding {:?} in the below", config.query);
@@ -61,6 +79,24 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
+pub fn search_insensitive<'lifetime>(query: &str, contents: &'lifetime str) -> Vec<&'lifetime str> {
+    // convert query to lowercase
+
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        // don't convert the whole content to lowercase but just the line you are on while iterating and compare
+        if line.to_lowercase().contains(&query) {
+            // do something with line
+            results.push(line);
+            println!("Found in line: {line}")
+        }
+    }
+
+    results
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -79,13 +115,25 @@ Pick three.";
 
     #[test]
     fn case_sensitive() {
-        let query = "e will";
+        let query = "e wilL";
         let contents = "This is the sentence in which
 the code wiLl try to 
 find query.";
 
         let s = search(query, contents);
 
-        println!("{:?}", s);
+        println!("Found data: {:?}", s);
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "e wilL";
+        let contents = "This is the sentence in which
+the code wiLl try to 
+find query.";
+
+        let s = search_insensitive(query, contents);
+
+        println!("Found data: {:?}", s);
     }
 }
